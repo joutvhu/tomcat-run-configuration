79b929187befb6b330f51229692396b64d6abb70
Giao Ho <joutvhu@gmail.com>
2025-03-21 04:09:32 +0700

    Fix setenv.bat

--- setenv.bat
+++ setenv.bat
@@ -1,25 +1,31 @@
+@echo off
+rem Change to the appropriate JDK
 set "JAVA_HOME=%JAVA_HOME%"
 
+rem Read MV Options
+setlocal enabledelayedexpansion
 set vmoptsFile=%cd%\.vmoptions
+set "vmoptions="
 if exist "%vmoptsFile%" (
-    set "vmoptions="
     for /f "tokens=* delims=" %%a in (%vmoptsFile%) do (
         set "vmopt=%%a"
         set "vmoptions=!vmoptions! !vmopt!"
     )
     set "vmoptions=!vmoptions:~1!"
-
-    set "JAVA_OPTS=%JAVA_OPTS% %vmoptions%"
 )
+endlocal && set vmoptions=%vmoptions%
+set "JAVA_OPTS=%JAVA_OPTS%%vmoptions%"
 
+rem Read Catalina Options
+setlocal enabledelayedexpansion
 set catoptsFile=%cd%\.catalinaoptions
+set "catoptions="
 if exist "%catoptsFile%" (
-    set "catoptions="
     for /f "tokens=* delims=" %%a in (%catoptsFile%) do (
         set "caopt=%%a"
         set "catoptions=!catoptions! !caopt!"
     )
     set "catoptions=!catoptions:~1!"
-
-    set "CATALINA_OPTS=%CATALINA_OPTS% %catoptions%"
 )
+endlocal && set catoptions=%catoptions%
+set "CATALINA_OPTS=%CATALINA_OPTS%%catoptions%"
